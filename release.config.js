59641b82a8097f5f6b2d0f88806818b87df38c75
rwxproject <rwxproject@gmail.com>
2021-07-06 10:56:00 -0400

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+modules.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/rwxproject/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/git"
+  ]
+}
